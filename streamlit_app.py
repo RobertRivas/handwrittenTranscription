732cdde0b0b8efa53971306bce2e0219dedc2875
robertrivas <robrrivas@gmail.com>
2024-06-05 03:33:41 -0700

    removed multiple widget error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write('Upload a pdf handwritten or text.')
 
 # lm.llm_function()
 
-file_upload = st.file_uploader('Choose your file', type="pdf")
+file_upload = st.file_uploader('Choose your file', type="pdf", key='upload')
 
 if file_upload is not None:
     print(file_upload)
